ee7d3c5c0c0fbada9310c83b06707225aeb6ed31
Arun <123rjk04@gmail.com>
2024-10-26 15:04:30 +0530

    second commit

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Avatar from '../images/avatar1.JPG'
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../images/avatar1.JPG";
 
 const PostAuthor = () => {
   return (
-    <div className='post__link'>
-        <Link to={`/posts/users/sdfsdf`} className='post__author'>
-        <div className='post__author-avatar'>
-            <img src={Avatar} alt="" />
+    <div className="post__link">
+      <Link to={`/posts/users/sdfsdf`} className="post__author">
+        <div className="post__author-avatar">
+          <img src={Avatar} alt="" />
         </div>
-        <div className='post__author-details'>
-            <h1>By: Arun Choudhary</h1>
-            <small>Just Now</small>
+        <div className="post__author-details">
+          <h1>By: Arun Choudhary</h1>
+          <small>Just Now</small>
         </div>
-    </Link>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default PostAuthor
+export default PostAuthor;
