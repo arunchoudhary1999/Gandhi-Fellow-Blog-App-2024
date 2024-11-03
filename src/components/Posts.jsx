67f67bcd1fa6cec4523125098d1920de0ebca3c5
Arun <123rjk04@gmail.com>
2024-11-03 12:29:07 +0530

    fifth commit

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,22 +1,124 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import PostItem from '../components/PostItem'
+import PostItem from "../components/PostItem";
 
-import { DUMMY_POSTS } from '../data';
+import { DUMMY_POSTS } from "../data";
 
 const Posts = () => {
-
-    const [posts, setPosts] = useState(DUMMY_POSTS)
+  const [posts, setPosts] = useState(DUMMY_POSTS);
 
   return (
-    <section className='posts'>
-        {posts.length > 0 ? <div className='container posts__container'>
-        {
-            posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
-        }
-        </div> : <h2 className='center'>No Posts Founds</h2>}
+    <section className="posts">
+      <h1
+        style={{
+          textAlign: "center",
+          background: "red",
+          width: "83%",
+          margin: "20px auto",
+          padding: "20px 0",
+          fontSize: "2rem",
+        }}
+      >
+        🕯️{" "}
+        <spam
+          style={{
+            color: "white",
+          }}
+        >
+          H
+        </spam>
+        <spam
+          style={{
+            color: "blue",
+          }}
+        >
+          a
+        </spam>
+        <spam
+          style={{
+            color: "gold",
+          }}
+        >
+          p
+        </spam>
+        <spam
+          style={{
+            color: "neon",
+          }}
+        >
+          p
+        </spam>
+        <spam
+          style={{
+            color: "skyblue",
+          }}
+        >
+          y
+        </spam>
+        <spam
+          style={{
+            color: "black",
+          }}
+        >
+          {" "}
+          D
+        </spam>
+        <spam
+          style={{
+            color: "brown",
+          }}
+        >
+          i
+        </spam>
+        <spam
+          style={{
+            color: "orange",
+          }}
+        >
+          w
+        </spam>
+        <spam
+          style={{
+            color: "pink",
+          }}
+        >
+          a
+        </spam>
+        <spam
+          style={{
+            color: "green",
+          }}
+        >
+          l
+        </spam>
+        <spam
+          style={{
+            color: "yellow",
+          }}
+        >
+          i
+        </spam>
+        🕯️
+      </h1>
+      {posts.length > 0 ? (
+        <div className="container posts__container">
+          {posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
+            <PostItem
+              key={id}
+              postID={id}
+              thumbnail={thumbnail}
+              category={category}
+              title={title}
+              desc={desc}
+              authorID={authorID}
+            />
+          ))}
+        </div>
+      ) : (
+        <h2 className="center">No Posts Founds</h2>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
